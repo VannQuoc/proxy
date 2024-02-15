9f15b7ebd2a8c27408be313a0fc9d7bfc71f28d0
VannQuoc <157780365+VannQuoc@users.noreply.github.com>
2024-02-15 20:06:32 +0700

    Update main.py

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-from flask import Flask, send_file
+from flask import Flask, send_file, Response
 import threading
 import time
 import requests
@@ -37,13 +37,55 @@ def main():
                         http_file.write(f'{proxy}\n')
             except requests.RequestException:
                 pass
+        with open('proxy.txt', 'r') as input_file:
+            proxy = input_file.read()
+        with open('live.txt', 'w') as output_file:
+            output_file.write(proxy)
         time.sleep(300)
 #FLASK
-@app.route('/get_proxy', methods=['GET'])
+@app.route('/proxy', methods=['GET'])
 def get_proxy_route():
     try:
+        file_path = 'live.txt'
+        with open(file_path, 'r') as file:
+            file_content = file.read()
+        return Response(file_content, content_type='text/plain')
+    except Exception as e:
+        return str(e)
+@app.route('/new', methods=['GET'])
+def proxy_moi():
+    try:
         file_path = 'proxy.txt'
-        return send_file(file_path, as_attachment=True)
+        with open(file_path, 'r') as file:
+            file_content = file.read()
+        return Response(file_content, content_type='text/plain')
+    except Exception as e:
+        return str(e)
+@app.route('/only', methods=['GET'])
+def mot_proxy():
+    try:
+        file_path = 'proxy.txt'
+        with open(file_path, 'r') as file:
+            first_line = file.readline().strip()
+        return Response(first_line, content_type='text/plain')
+    except Exception as e:
+        return str(e)
+@app.route('/nocheck', methods=['GET'])
+def proxy_chua_check():
+    try:
+        file_path = 'http.txt'
+        with open(file_path, 'r') as file:
+            file_content = file.read()
+        return Response(file_content, content_type='text/plain')
+    except Exception as e:
+        return str(e)
+@app.route('/', methods=['GET'])
+def index():
+    try:
+        file_path = 'README.md'
+        with open(file_path, 'r') as file:
+            file_content = file.read()
+        return Response(file_content, content_type='text/plain')
     except Exception as e:
         return str(e)
 
